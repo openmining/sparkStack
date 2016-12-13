d4e84af62e69e1ace1dafae88ee9accd2465a50c
openmining <openmining@users.noreply.github.com>
2016-12-14 07:42:02 +0900

    Update e200CreateOrReplaceTempView.scala

--- src/main/java/spark/e200/stack/e200CreateOrReplaceTempView.scala
+++ src/main/java/spark/e200/stack/e200CreateOrReplaceTempView.scala
@@ -1,4 +1,4 @@
-package spark.e200.stack
+package spark.openmining.stack
 
 object e200CreateOrReplaceTempView {
 	def main(args: Array[String]){
@@ -7,21 +7,19 @@ object e200CreateOrReplaceTempView {
       .setMaster("local")
     val sc = new org.apache.spark.SparkContext(conf)
     val spark = new org.apache.spark.sql.SQLContext(sc)
-    
-    // Column Type 추론    
-
+     
     val dfInput = spark.read.format("csv")
-      .option("header","true")
-      .option("inferSchema","true")
-      .load("data/TitanicTrain.csv")
+      .option("header","true")  // get Column Name
+      .option("inferSchema","true")  // Column Type 추론 
+      .load("data/GDPbyCountry.csv")
     System.out.print(dfInput)
     dfInput.show()
     dfInput.printSchema
-    dfInput.createOrReplaceTempView("TitanicTrain")
-    val qDfInputNObs = spark.sql("select count(*) as NumOfObs from TitanicTrain")
+    dfInput.createOrReplaceTempView("GDPbyCountry")
+    val qDfInputNObs = spark.sql("select count(*) as NumOfObs from GDPbyCountry")
     qDfInputNObs.show()
     
-    val qDfInputTopN = spark.sql("select * from TitanicTrain limit 10")
+    val qDfInputTopN = spark.sql("select * from GDPbyCountry limit 10")
     qDfInputTopN.show()
     qDfInputTopN.printSchema()
 
