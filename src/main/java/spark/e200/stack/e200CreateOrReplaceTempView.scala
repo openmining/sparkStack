7838dd06c8f92fa0ce5e872ea9e7576bdcbbbd87
openmining <openmining@users.noreply.github.com>
2016-12-14 00:20:29 +0900

    Create e200CreateOrReplaceTempView.scala

---
+++ src/main/java/spark/e200/stack/e200CreateOrReplaceTempView.scala
@@ -0,0 +1,29 @@
+package spark.e200.stack
+
+object e200CreateOrReplaceTempView {
+	def main(args: Array[String]){
+    val conf = new org.apache.spark.SparkConf()
+      .setAppName("e200SessionJson")
+      .setMaster("local")
+    val sc = new org.apache.spark.SparkContext(conf)
+    val spark = new org.apache.spark.sql.SQLContext(sc)
+    
+    // Column Type 추론    
+
+    val dfInput = spark.read.format("csv")
+      .option("header","true")
+      .option("inferSchema","true")
+      .load("/Home/OneDrive/data/TitanicTrain.csv")
+    System.out.print(dfInput)
+    dfInput.show()
+    dfInput.printSchema
+    dfInput.createOrReplaceTempView("TitanicTrain")
+    val qDfInputNObs = spark.sql("select count(*) as NumOfObs from TitanicTrain")
+    qDfInputNObs.show()
+    
+    val qDfInputTopN = spark.sql("select * from TitanicTrain limit 10")
+    qDfInputTopN.show()
+    qDfInputTopN.printSchema()
+
+  }  
+}
